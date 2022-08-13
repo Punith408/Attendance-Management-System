090ec856f5d7da3d0ba5b095118f85fc6ec8d5dd
Punith <punithshetty408@gmail.com>
2022-08-13 13:50:27 +0530

    "actually finally done"

--- backend/server/model/model.js
+++ backend/server/model/model.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose')
 
 var schema = new mongoose.Schema({
-    // sl:{
-    //     type: Number,
-    //     required : true
-    // },
+   
     name : {
         type : String,
         required :true,
@@ -21,32 +18,9 @@ var schema = new mongoose.Schema({
         required:true
 
     },
-    // total:{
-    //     type:Number,
-    //     required:true
-
-    // }
+  
 })
-// var Attendance_schema = new mongoose.Schema({
-//     at:{
-//         type: Boolean,
-//         required:true
-//     },
-//     status:{
-//         type:Number
-//     }
-// })
-// var schema2 = new mongoose.Schema({
-//     username: {
-//         type : String,
-//         required :true,
-//         unique: true
-//     },
-//     password:{
-//         type : String,
-//         required: true
-//     }
-// })
+
 const Userdb = mongoose.model('userdb',schema);
 // const teacher_login = mongoose.model('teacher_login',schema);
 module.exports = Userdb;
